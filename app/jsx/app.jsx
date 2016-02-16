ff4f940d5f783476a1019006fe4622271ed8ef2e
unknown <isilakov85@gmail.com>
2016-02-16 08:08:59 -0800

    some change

--- app/jsx/app.jsx
+++ app/jsx/app.jsx
@@ -3,14 +3,17 @@
 define([], function () {
 	return React.createClass({
 		getInitialState: function() {
-			return {message: 'Hello World!'};
+			return {message: 'Hello World!', message1: 'Hell or World!'};
 		},
 		goodbye: function(event) {
 			this.setState({message: 'Goodbye World.'});
 		},
 		render: function() {
 			return (
-				<h1 onClick={this.goodbye}>{this.state.message}</h1>
+				<div>
+					<h2>{this.state.message}</h2>
+					<h1 class='asd'>{this.state.message1}</h1>
+				</div>
 			);
 		}
 	});
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,17 +3,16 @@
 define([], function () {
 	return React.createClass({
 		getInitialState: function() {
-			return {message: 'Hello World!', test: 'are you here'};
+			return {message: 'Hello World!', message1: 'Hell or World!'};
 		},
 		goodbye: function(event) {
 			this.setState({message: 'Goodbye World.'});
 		},
 		render: function() {
 			return (
-				React.DOM.div(
-					null, 
-					React.DOM.h1( null, "Text1"),
-					React.DOM.h2( null, this.state.message)
+				React.DOM.div(null, 
+					React.DOM.h2(null, this.state.message),
+					React.DOM.h1( {class:"asd"}, this.state.message1)
 				)
 			);
 		}
